86bb82cd3d5b560206b058a8ca83d4b4098b844a
Forever <lynk0613@hotmail.com>
2018-07-09 13:11:21 -0400

    add node server

---
+++ my-table/server.js
@@ -0,0 +1,77 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var path = require('path')
+
+var app = express()
+
+// Allow all requests from all domains & localhost
+app.all('/*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'POST, GET')
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+const rowsTemplate = [
+  {
+    'id': '234kjw',
+    'email': 'abcdefg@gmail.com',
+    'name': 'Alex',
+    'phone': '1231231231',
+    'adress': '43 DS SSS'
+  },
+  {
+    'id': '1233ds',
+    'email': 'jddsfhsd@aadw.com',
+    'name': 'John',
+    'phone': '23463421',
+    'adress': 'dss DS SSS'
+  },
+  {
+    'id': 'hjsd2d',
+    'email': 'sdffgsdf@43.com',
+    'name': 'Pavle',
+    'phone': '63426234',
+    'adress': 'AAD sdsdf SSS'
+  },
+  {
+    'id': 'dhdsaf',
+    'email': 'fgsdshsd@1ss.com',
+    'name': 'Xin',
+    'phone': '89546464',
+    'adress': '2 DS 2'
+  },
+  {
+    'id': 'otdfgs',
+    'email': 'vsdfs@sdsdf.com',
+    'name': 'Ahmed',
+    'phone': '32422344',
+    'adress': '6 DS 4'
+  }
+]
+app.use(express.static(path.join(__dirname, '/dist/spa-mat/')))
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(`${__dirname}/dist/spa-mat/index.html`))
+})
+
+
+app.get('/api', function (req, res) {
+  // let page = req.param('page')
+  // let rowPerPage = req.param('row_per_page')
+  let rows = req.param('rows')
+  let result = []
+  for (let i = 0; i < rows; i++) {
+    let index = parseInt(Math.random() * rowsTemplate.length)
+    result.push(rowsTemplate[index])
+  }
+  res.send(result)
+})
+
+
+app.listen(3000)
+console.log('Running at Port 3000')
+
--- my-table/src/components/myTable.vue
+++ my-table/src/components/myTable.vue
@@ -1,19 +1,6 @@
 <template>
 
-  <q-scroll-area
-    @scroll="scrolling"
-    ref="scrollArea"
-    :style="`width: 100%; height: ${rowHeight*serverPagination.rowsPerPage}px; margin-top: 60px;`"
-    :thumb-style="{
-      right: '4px',
-      borderRadius: '5px',
-      background: 'red',
-      width: '10px',
-      opacity: 1
-    }"
-    :delay="0"
-  >
-    <div ref="virtualTop"/>
+<div style="width: 100%">
     <q-table
       ref="table"
       :data="serverData"
@@ -23,8 +10,38 @@
       :loading="loading"
       >  
     </q-table>
+    
+  <q-scroll-area
+    @scroll="scrolling"
+    ref="scrollArea"
+    :style="`width: 100%; height: ${rowHeight*serverPagination.rowsPerPage}px;`"
+    :thumb-style="{
+      right: '4px',
+      borderRadius: '5px',
+      background: 'red',
+      width: '10px',
+      opacity: 1
+    }"
+    :delay="1" >
+    <div v-for="(fi, index) in freezeRows" :key="index + '-freezeCell'" :style="`height:${rowHeight}px; display: flex; position: fixed; width: 100%; margin-top: ${(index) * rowHeight}px; background-color: #e6e6e6;`" class="q-row">
+      <div v-for="col in columns" :key="col.name" :style="`width:${col.width}; background-color: #e6e6e6;`" class="q-cell">
+        {{collectedData[index][col.name]}}
+      </div>
+    </div>
+
+    <div ref="virtualTop"/>
+    
+    <div v-for="(row, index) in serverData" :key="index" :style="`height:${rowHeight}px; display: flex;`" class="q-row">
+      <div v-for="col in columns" :key="col.name" :style="`width:${col.width}; background-color: #ffffff;`" class="q-cell">
+        {{row[col.name]}}
+      </div>
+    </div>
+    
     <div ref="virtualBottom"/>
+
   </q-scroll-area>
+</div>
+
 </template>
 
 <script>
@@ -34,10 +51,12 @@ export default {
     return {
       loading: false,
       requestSource: null,
+      requestSource2: null,
       serverPagination: {
         page: 1,
-        rowsPerPage: 5,
-        rowsNumber: 30 // specifying this determines pagination is server-side
+        rowsPerPage: 6,
+        rowsNumber: 60, // specifying this determines pagination is server-side
+        rows: 12
       },
       rowHeight: 40, 
       serverData: [],
@@ -46,17 +65,24 @@ export default {
       scrollOffset: 0,
       scrollUnit: 1,
       scrollUnitOffset: 0,
+      freezeRows: 1,
+      requestAPI: null,
+      requestAPI2: null,
+
+      isEnableScroll: true,
       columns: [
         {
-          name: 'email',
+          name: 'id',
           required: true,
-          label: 'E-Mail',
+          label: 'Id',
           align: 'left',
           field: 'email',
-          sortable: false
+          sortable: false,
+          width: '25%'
         },
-        { name: 'nat', label: 'Nation', field: 'nat', sortable: false },
-        { name: 'phone', label: 'Phone', field: 'phone', sortable: false }
+        { name: 'email', label: 'E-Mail', field: 'email', sortable: false, width: '25%' },
+        { name: 'name', label: 'Name', field: 'name', sortable: false, align: 'left', width: '25%' },
+        { name: 'phone', label: 'Phone', field: 'phone', sortable: false, align: 'left', width: '25%' }
 
       ]
     }
@@ -66,12 +92,13 @@ export default {
       this.loading = true
       const CancelToken = axios.CancelToken
       const source = CancelToken.source()
+      console.log('request')
       const promise = axios
-        .get(`https://randomuser.me/api/?results=${pagination.rowsPerPage * 2}&page=${Math.ceil(pagination.page / 2.0)}`, {cancelToken: source.token})
+        .get(`/api/?row_per_page=${pagination.rowsPerPage}&page=${pagination.page}&rows=${pagination.rowsPerPage * 2}`, {cancelToken: source.token})
         .then(({ data }) => {
           for (let i = 0; i < pagination.rowsPerPage * 2; i++) {
-            let index = (Math.ceil(pagination.page / 2.0) - 1) * pagination.rowsPerPage * 2 + i 
-            if (this.collectedData[index]['email'] !== data.results[i]['email']) {
+            let index = pagination.rowsPerPage * (pagination.page - 1)  + i 
+            if (!this.collectedData[index]['id']) {
               this.collectedData[index] = data.results[i]
             }
           }
@@ -79,9 +106,9 @@ export default {
           this.loading = false
         })
         .catch(error => {
-          // there's an error... do SOMETHING
-          // we tell QTable to exit the "loading" state
+          console.log('error', error)
           this.loading = false
+          this.renderData()
         })
 
       return {
@@ -91,78 +118,87 @@ export default {
 
 
     renderData() {
-      let tempData = [{}]
+      console.log("currentPage", this.currentPage + 1)
+      let tempData = []
       let index = this.currentPage * this.serverPagination.rowsPerPage
       for (let i = 0; i < this.serverPagination.rowsPerPage * 2 ; i++) {
         tempData.push(this.collectedData[index + i])
       }
       this.serverData = tempData
 
-      setTimeout(() => {
-        let tr = this.$el.querySelectorAll('tr')
-        for (let i = 2; i < tr.length ; i++) {
-            tr[i].style.height = `${this.rowHeight}px`
-        }
+      let vTop = this.currentPage * this.rowHeight * this.serverPagination.rowsPerPage
+      this.$refs['virtualTop'].style.height = `${vTop}px`
+      this.$refs['virtualBottom'].style.height = `${(this.serverPagination.rowsNumber - this.serverData.length) * this.rowHeight -vTop}px`
 
-        let vTop = this.currentPage * this.rowHeight * this.serverPagination.rowsPerPage
-        //this.$refs['virtualTop'].style.height = `${vTop}px`
-        tr[1].style.height = `${vTop}px`
-        tr[1].style.display = this.currentPage === 0 ? 'none' : 'block'
-        this.$refs['virtualBottom'].style.height = `${(this.serverPagination.rowsNumber - this.serverData.length + 1) * this.rowHeight -vTop}px`
-
-      }, 0);
 
     },
 
 
     scrolling (e) {
+      
       if (e.path[1] == this.$refs['scrollArea'].$el){
+        if (!this.isEnableScroll) return
+        this.isEnableScroll = false
+        setTimeout(()=>{
+          this.isEnableScroll = true
+        },60)
+        
         this.currentPage = parseInt(e.path[0].scrollTop / e.path[0].scrollHeight * (this.serverPagination.rowsNumber * 1.0 / this.serverPagination.rowsPerPage))
+        if (this.currentPage >= this.serverPagination.rowNumber/this.serverPagination.rowsPerPage) this.currentPage = this.serverPagination.rowNumber/this.serverPagination.rowsPerPage - 1 
         this.scrollUnit = e.path[0].scrollHeight * 1.0 / this.serverPagination.rowsNumber
         this.scrollUnitOffset = (e.path[0].scrollTop * 1000 % (this.scrollUnit * 1000)) / 1000.0
         this.scrollOffset = e.path[0].scrollTop % (this.scrollUnit * this.serverPagination.rowsPerPage)
 
-        console.log(this.scrollOffset, this.currentPage)
         let index = this.currentPage * this.serverPagination.rowsPerPage
 
-        console.log(index)
+        console.log(this.currentPage, this.collectedData[index]['email'])
+
         let rendered = false
-        if (!this.collectedData[index]['email']) {
+        if (index < this.serverPagination.rowsNumber  &&
+          !this.collectedData[index]['email']) {
+            
           rendered = true
           if (this.requestSource) {
             this.requestSource.cancel()
           }
-          const request= this.request({
-            pagination: {
-              page: this.currentPage + 1,
-              rowsPerPage: this.serverPagination.rowsPerPage,
-              rowsNumber: this.serverPagination.rowsNumber
 
-            },
-          })
-          this.requestSource = request.source
+          setTimeout(()=>{
+            this.requestAPI = this.request({
+              pagination: {
+                page: this.currentPage + 1,
+                rowsPerPage: this.serverPagination.rowsPerPage,
+                rowsNumber: this.serverPagination.rowsNumber,
+                rows: this.serverPagination.rowsPerPage * 2
+              },
+            })
+            this.requestSource = this.requestAPI.source
+          }, 10)
+
           
-        }
-        if (index + this.serverPagination.rowsPerPage < this.serverPagination.rowsNumber  &&
+        }  
+        
+        if (index + this.serverPagination.rowsPerPage <= this.serverPagination.rowsNumber  &&
           !this.collectedData[index + this.serverPagination.rowsPerPage]['email']) {
           rendered = true
-          if (this.requestSource) {
-            this.requestSource.cancel()
+         
+          if (this.requestSource2) {
+            this.requestSource2.cancel()
           }
-          const request= this.request({
-            pagination: {
-              page: this.currentPage + 1 + 1,
-              rowsPerPage: this.serverPagination.rowsPerPage,
-              rowsNumber: this.serverPagination.rowsNumber
-
-            },
-          })
-          this.requestSource = request.source
+          setTimeout(()=>{
+            this.requestAPI2 = this.request({
+              pagination: {
+                page: this.currentPage + 1 + 1,
+                rowsPerPage: this.serverPagination.rowsPerPage,
+                rowsNumber: this.serverPagination.rowsNumber,
+                rows: this.serverPagination.rowsPerPage * 2
+              },
+            })
+            this.requestSource2 = this.requestAPI2.source
+          }, 10)
  
         }
-        if (!rendered) {
-          this.renderData()
-        }
+        
+        this.renderData()
       }
     }
   },
@@ -174,14 +210,13 @@ export default {
   },
   mounted () {
     this.collectedData = []
-    for (let i = 0; i < this.serverPagination.rowsNumber; i ++) {
-      this.collectedData.push({email:null, nat:'...', phone:'...'})
+    for (let i = 0; i < this.serverPagination.rowsNumber + this.serverPagination.rowsPerPage * 2; i ++) {
+      this.collectedData.push({id:null, email:'' , name:'', phone:''})
     }
     const request = this.request({
       pagination: this.serverPagination,
     })
     this.requestSource = request.source
-
 
   },
 
@@ -191,29 +226,19 @@ export default {
 </script>
 <style>
   tbody {
-      display:block;
-      overflow:auto;
-  }
-  thead, tbody tr {
-      display:table;
-      width:100%;
-      table-layout:fixed;/* even columns width , fix width of table too*/
-  }
-  thead {
-      width: calc( 100% - 1em );/* scrollbar is average 1em/16px width, remove it from thead width */
-      position: fixed;
-      margin-top: -60px;
-  }
-  table {
-      width:400px;
-  }
-  .q-table tbody td {
-    height: 0px !important;
+    display: none;
   }
   .q-table-bottom {
     display: none;
   }
-  .q-scrollarea-thumb {
-    transition: none !important;
+  .q-row {
+    border-bottom: 1px solid rgba(0,0,0,.12)
+  }
+  .q-cell {
+    display: flex;
+    align-content: center;
+    padding: 10px;
+    font-weight: 400;
+    font-size: 13px;
   }
 </style>
